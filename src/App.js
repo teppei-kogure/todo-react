7a1f068dee2773d34c96921a843a3451455afaef
teppei-kogure <ipetsute@live.jp>
2022-01-25 15:43:04 +0900

    single component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,62 @@ import { React, useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
-  const [completeTodos, setcompleteTodos] = useState(["ううう"]);
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value); // event.target.valueにはinputの値が入る
+  };
+
+  const onClickComplete = (index) => {
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    onClickDelete(index);
+  };
+  const onClickDelete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+  const onClickReturn = (index) => {
+    const newTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newTodos);
+
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
-              <li>
-                <div key={todo} className="list-row">
+              <li key={todo}>
+                <div className="list-row">
                   {/*  Reactでループ処理をする場合、仮想DOMの目標としてkeyを設定する必要がある*/}
                   <p className="todo">{todo}</p>
-                  <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
+                  {/*関数に引数を渡したいときはアロー関数する必要がある。そうしないと、すぐに実行されてしまう*/}
+                  <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               </li>
             );
@@ -30,12 +67,12 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了したTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
-              <li>
-                <div key={todo} className="list-row">
+              <li key={todo}>
+                <div className="list-row">
                   <p className="todo">{todo}</p>
-                  <button>戻す</button>
+                  <button onClick={() => onClickReturn(index)}>戻す</button>
                 </div>
               </li>
             );
